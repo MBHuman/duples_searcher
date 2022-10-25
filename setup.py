0e5ee1a221f5de4eb3cc4432c1e2746dc54b8038
Evgeny <zenka712@mail.ru>
2022-10-25 17:25:21 +0300

    add readme test

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ from searcher.version import __version__
 setup(
     name='duples_searcher',
     version=__version__,
+    long_description="README.md",
+    long_description_content_type="text/markdown",
     description='Search information about duplicates in files',
     author='Evgeny Dmitriev',
     author_email="zenka712@mail.ru",
